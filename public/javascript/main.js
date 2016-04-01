2a4f5bfad15e0b36927c55b6478a43220ed34885
Griffin Anderson <griffinanderson@Griffins-MacBook-Pro.local>
2016-04-01 19:41:54 -0400

    small changes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,9 @@
-$( "#anagram" ).submit(function( event ) {
-  alert( "Handler for .submit() called." );
+$( "form" ).submit(function( event ) {
+  if ( $( "input:first" ).val() === "correct" ) {
+    $( "span" ).text( "Validated..." ).show();
+    return;
+  }
+ 
+  $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
   event.preventDefault();
 });
