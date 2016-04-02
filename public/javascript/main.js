0aa99f2ff0a40c2acc39a7792a120ce58bf115ff
Griffin Anderson <griffinanderson@Griffins-MacBook-Pro.local>
2016-04-01 20:47:18 -0400

    small changes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,12 +2,12 @@ $( "form" ).submit(function( event ) {
     
   event.preventDefault();
 
-  var firstString = $( "#first_string" ).val().split().sort().join();
-  var secondString = $( "#second_string" ).val().split().sort().join();      
+  var firstString = $( "#first_string" ).val().split(",").sort().join(",");
+  var secondString = $( "#second_string" ).val().split(",").sort().join(",");      
     
-  alert($( "#first_string" ).val().split());
-    alert($( "#first_string" ).val().split().sort());
-    alert($( "#first_string" ).val().split().sort().join());
+  alert($( "#first_string" ).val().split(","));
+    alert($( "#first_string" ).val().split(",").sort());
+    alert($( "#first_string" ).val().split(",").sort().join(","));
   alert(secondString);    
     
   if ( firstString === secondString) {
