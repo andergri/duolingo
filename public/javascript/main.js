6f6cf338ced68e11b9451e094224bd83b9bee954
Griffin Anderson <griffinanderson@Griffins-MacBook-Pro.local>
2016-04-01 20:24:47 -0400

    small changes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,8 @@
 $( "form" ).submit(function( event ) {
     
   event.preventDefault();
+
+  getSortedString( $( "#first_string" ).val() );    
     
   if ( $( "#first_string" ).val() === $( "#second_string" ).val() ) {
     $( "span" ).text( "Valid Anagrams" ).show().fadeOut( 3000 );
@@ -8,8 +10,20 @@ $( "form" ).submit(function( event ) {
   }
  
   $( "span" ).text( "Not valid!" ).show().fadeOut( 3000 );
-
-    
- alert("222");
     
 });
+
+function getSortedString(str) {
+    
+    str = str.split('');
+
+    str = str.sort(function (a,b) {
+        if (a === '0' || b === '0')
+            return (b === a) ?  0 : (a < b) ? 1 : -1;
+        return (a < b) ? -1 : (a === b) ? 0 : 1;
+    });
+
+    alert(str);
+    
+    return str;
+}
